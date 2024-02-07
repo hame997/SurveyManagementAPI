8e6bd9a4da7ae90f9475516ee437c942c82c4bd2
hame997 <muhamed.ajanovic@edu.fit.ba>
2024-02-07 16:57:17 +0100

    Implementation API for surveyManagement and Added assistant necessary class

---
+++ SurveyManagementAPI/Models/SurveyManagement.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+public class SurveyManagement
+{
+    private readonly MojDbContext _context;
+
+    public SurveyManagement(MojDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> SubmitSurveyResponse(int surveyId, SurveyResponse response)
+    {
+        // Implementiraj logiku za dodavanje odgovora na anketu u bazu podataka
+        // Ovdje možeš validirati odgovore i provjeriti da li su svi potrebni odgovori dostupni
+
+        // Primjer logike:
+        var survey = await _context.Surveys.Include(s => s.Questions).FirstOrDefaultAsync(s => s.Id == surveyId);
+        if (survey == null)
+        {
+            return false; // Anketa nije pronađena
+        }
+
+        // Provjeri da li je korisnik odgovorio na sva pitanja
+        if (response.Answers.Count != survey.Questions.Count)
+        {
+            return false; // Korisnik nije odgovorio na sva pitanja
+        }
+
+        // Spremi odgovore na anketu u bazu podataka
+        _context.SurveyResponses.Add(response);
+        await _context.SaveChangesAsync();
+
+        return true;
+    }
+
+    public async Task<IEnumerable<SurveyResult>> GetSurveyResults(int surveyId)
+    {
+        // Implementiraj logiku za dobijanje trenutnih rezultata ankete
+        // Samo administratori mogu pristupiti ovoj metodi
+
+        // Primjer logike:
+        var survey = await _context.Surveys.Include(s => s.Questions).FirstOrDefaultAsync(s => s.Id == surveyId);
+        if (survey == null)
+        {
+            return null; // Anketa nije pronađena
+        }
+
+        // Ovdje možeš implementirati logiku za računanje rezultata ankete na osnovu odgovora u bazi podataka
+        // Vrati rezultate ankete kao IEnumerable<SurveyResult>
+
+        return null;
+    }
+}
